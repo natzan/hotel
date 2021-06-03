2cfc5d2e328f48c0a15c429611b1baa51de69f74
Valdorion <70708301+Valdorion@users.noreply.github.com>
2021-06-03 15:10:27 +0200

    Update connexion.php

--- Hotel/connexion.php
+++ Hotel/connexion.php
@@ -1,7 +1,7 @@
 <?php    /*   CONNEXION A LA BASE*/
 try
 {   
-    $bdd = new PDO('mysql:host=localhost;dbname=hotel;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO("mysql:host=10.10.20.25;dbname=hotel;charset=utf8", 'nsi', 'nsi', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	//var_dump($bdd);
 }
 catch(Exception $e)
